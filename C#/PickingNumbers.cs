41e4e9f3c59aee5dbc1ffb534d6c45106728f356
nguyenhuy-iot <nguyenhuy.iot@gmail.com>
2021-02-09 21:41:02 +0700

    Picking Numbers

---
+++ C#/PickingNumbers.cs
@@ -0,0 +1,63 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+    /*
+     Given an array of integers, 
+    find the longest subarray where the absolute difference between any two elements is less than or equal to 1.
+     */
+    /*
+     * Complete the 'pickingNumbers' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY a as parameter.
+     */
+
+    public static int pickingNumbers(List<int> a)
+    {
+        int[] f = new int[101];
+        int max = 0;
+        foreach (int i in a)
+        {
+            f[i]++;
+        }
+        for (int i = 0; i < 100; i++)
+        {
+            max = Math.Max(f[i] + f[i + 1], max);
+        }
+        return max;
+
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+
+        int result = Result.pickingNumbers(a);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
