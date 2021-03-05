76c1ad65a9873842875795104f1384152c97a90b
nguyenhuy-iot <nguyenhuy.iot@gmail.com>
2021-03-05 21:18:43 +0700

    Tool_directory

---
+++ Tool/Tool_directory/Tool_directory/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tool_directory
+{
+    public partial class Form1 : Form
+    {
+        void NewFoder(string directoryPath)
+        {            
+            DirectoryInfo directory = new DirectoryInfo(directoryPath);
+            if (!directory.Exists)
+            {
+                directory.Create();
+            }
+            tbxLink.Text = directoryPath;
+        }
+        void ImagePaste(string FilePath)
+        {
+            try
+            {
+                Image IMG = Clipboard.GetImage();
+                if (IMG != null)
+                {
+                    IMG.Save(FilePath);
+                    tbxLink.Text = FilePath;
+                }
+                else
+                {
+                    MessageBox.Show("not Image");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("ERROR");
+            }
+            
+        }
+        void CopyAll(string sourcePath, string targetPath)
+        {
+            try
+            {
+                // To copy all the files in one directory to another directory.
+                if (System.IO.Directory.Exists(sourcePath))
+                {
+                    string[] files = System.IO.Directory.GetFiles(sourcePath);
+                    string fileName = "";
+                    // Use Path class to manipulate file and directory paths.
+                    string destFile = System.IO.Path.Combine(targetPath, fileName);
+                    // Copy the files and overwrite destination files if they already exist.
+                    foreach (string s in files)
+                    {
+                        // Use static Path methods to extract only the file name from the path.
+                        fileName = System.IO.Path.GetFileName(s);
+                        destFile = System.IO.Path.Combine(targetPath, fileName);
+                        System.IO.File.Copy(s, destFile, true);
+                    }
+                    MessageBox.Show("Successfully Copied");
+                }
+                else
+                {
+                    MessageBox.Show("Source path does not exist!");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("ERROR");
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAddFoder_Click(object sender, EventArgs e)
+        {
+            NewFoder(tbxPath.Text);           
+        }
+
+        private void btnIMG_Click(object sender, EventArgs e)
+        {
+            ImagePaste(tbxPath.Text + @"\" + tbxNameFile.Text);             
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            NewFoder(tbxPath.Text);
+            string FilePath = tbxPath.Text + @"\" + tbxNameFile.Text;
+            ImagePaste(FilePath);
+            tbxLink.Text = tbxPath.Text;
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            Process.Start(tbxLink.Text);
+        }
+
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            
+            string sourcePath = tbxSourcePath.Text;
+            string targetPath = tbxLink.Text;
+            CopyAll(sourcePath, targetPath);            
+        }
+    }
+}
